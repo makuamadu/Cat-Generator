8e0b0ee0b4e14f637762e27ccd72db85fd65febb
Makua Onuorah <61973944+makuamadu@users.noreply.github.com>
2021-06-23 11:42:27 +0100

    Add files via upload

---
+++ Cat Generator.js
@@ -0,0 +1,6 @@
+function Generate () {
+    var image =document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src ='./Cat.png' ;
+    div.appendChild(image);
+};
